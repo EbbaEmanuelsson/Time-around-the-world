473f65d9319b4f517c0f404eb92f64a930494504
EbbaEmanuelsson <ebbaemanuelsson97@gmail.com>
2023-05-22 21:39:10 +0200

    added the current location

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 setInterval(function () {
   let newYorkElement = document.querySelector("#new-york");
-  let newYorkDateElemant = newYorkElement.querySelector(".date");
-  let newYorkTimeElement = newYorkElement.querySelector(".time");
-  let newYorkTime = moment().tz("America/New_York");
+  if (newYorkElement){
+    let newYorkDateElemant = newYorkElement.querySelector(".date");
+    let newYorkTimeElement = newYorkElement.querySelector(".time");
+    let newYorkTime = moment().tz("America/New_York");
 
-  newYorkDateElemant.innerHTML = newYorkTime.format("dddd, MMMM Do YYYY");
-  newYorkTimeElement.innerHTML = newYorkTime.format("HH:mm:ss");
-
+    newYorkDateElemant.innerHTML = newYorkTime.format("dddd, MMMM Do YYYY");
+    newYorkTimeElement.innerHTML = newYorkTime.format("HH:mm:ss");
+  }
 
   let parisElement = document.querySelector("#paris");
-  let parisDateElemant = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
-  let parisTime = moment().tz("Europ/Paris");
+  if (parisElement){
+    let parisDateElemant = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
 
-  parisDateElemant.innerHTML = parisTime.format("dddd, MMMM Do YYYY");
-  parisTimeElement.innerHTML = parisTime.format("HH:mm:ss");
-  
+    parisDateElemant.innerHTML = parisTime.format("dddd, MMMM Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format("HH:mm:ss");
+  }
 
   let singaporeElement = document.querySelector("#singapore");
-  let singaporeDateElemant = singaporeElement.querySelector(".date");
-  let singaporeTimeElement = singaporeElement.querySelector(".time");
-  let singaporeTime = moment().tz("Asia/Singapore");
+  if(singaporeElement){
+    let singaporeDateElemant = singaporeElement.querySelector(".date");
+    let singaporeTimeElement = singaporeElement.querySelector(".time");
+    let singaporeTime = moment().tz("Asia/Singapore");
 
-  singaporeDateElemant.innerHTML = singaporeTime.format("dddd, MMMM Do YYYY");
-  singaporeTimeElement.innerHTML = singaporeTime.format("HH:mm:ss");
+    singaporeDateElemant.innerHTML = singaporeTime.format("dddd, MMMM Do YYYY");
+    singaporeTimeElement.innerHTML = singaporeTime.format("HH:mm:ss");
+  }
 }, 1000);
 
 function updateCity(event){
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "Current"){
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement =document.querySelector("#cities");
@@ -41,10 +48,7 @@ function updateCity(event){
     </div>
   
   `;
-
-
 }
-
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
